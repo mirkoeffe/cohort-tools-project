04fcf6f3534fe650d0d67ca8fc2300a97d791dca
Mirko Fede <mirko@mirkoeffe.shop>
2024-08-05 14:36:35 +0200

    add user model and authorization route

--- server/routes/cohorts.routes.js
+++ server/routes/cohorts.routes.js
@@ -1,57 +1,62 @@
 const router = require('express').Router();
-const { Types } = require('mongoose')
 
 const Cohorts = require('../models/cohorts.model')
-const Students = require('../models/students.model')
 
-router.post("/cohorts", (req, res, next) => {
-    Cohorts.create({
-        ...req.body,
-    })
-        .then((cohort) => {
-            console.log("Created new cohort:", cohort);
-            res.status(201).json(cohort);
-        })
-        .catch((error) => next(error));
+router.post("/cohorts", async (req, res, next) => {
+    try {
+        const cohort = await Cohorts.create({
+            ...req.body,
+        });
+        console.log("Created new cohort:", cohort);
+        res.status(201).json(cohort);
+    } catch (error) {
+        next(error);
+    }
 })
 
-router.get("/cohorts", (req, res, next) => {
-    Cohorts.find({})
-        .then((cohorts) => {
-            console.log(`Found ${cohorts.length}`);
-            res.json(cohorts);
-        })
-        .catch((error) => next(error));
+router.get("/cohorts", async (req, res, next) => {
+    try {
+        const cohorts = await Cohorts.find({});
+        console.log(`Found ${cohorts.length}`);
+        res.status(200).json(cohorts);
+    } catch (error) {
+        next(error);
+    }
 })
 
-router.get("/cohorts/:cohortId", (req, res, next) => {
-    Cohorts.findById(req.params.cohortId)
-        .then((cohort) => {
-            if (!cohort) return res.status(404).json({ message: "Cohort not found" });
-            res.json(cohort);
-        })
-        .catch((error) => next(error));
+router.get("/cohorts/:cohortId", async (req, res, next) => {
+    try {
+        const cohort = await Cohorts.findById(req.params.cohortId);
+        if (!cohort) return res.status(404).json({ message: "Cohort not found" });
+        res.status(200).json(cohort);
+    } catch (error) {
+        next(error);
+    }
 })
 
 router.put("/cohorts/:cohortId", (req, res, next) => {
     Cohorts.findByIdAndUpdate(req.params.cohortId, req.body, { new: true })
-        .then((updatedCohort) => {
-            if (!updatedCohort) return res.status(404).json({ message: "Cohort not found" });
+        .then(async (updatedCohort) => {
+            if (!updatedCohort) {
+                return res.status(404).json({ message: "Cohort not found" });
+            }
             console.log("Updated cohort:", updatedCohort);
 
-            res.json(updatedCohort);
+            res.status(200).json(updatedCohort);
         })
         .catch((error) => next(error));
 })
 
-router.delete("/cohorts/:cohortId", (req, res, next) => {
-    Cohorts.findByIdAndDelete(req.params.cohortId)
-        .then((deletedCohort) => {
-            if (!deletedCohort) return res.status(404).json({ message: "Cohort not found" });
-            console.log("Deleted cohort:", deletedCohort);
+router.delete("/cohorts/:cohortId", async (req, res, next) => {
+    try {
+        const deletedCohort = await Cohorts.findByIdAndDelete(req.params.cohortId);
+        if (!deletedCohort) return res.status(404).json({ message: "Cohort not found" });
+        console.log("Deleted cohort:", deletedCohort);
 
-            res.json(deletedCohort);
-        })
-        .catch((error) => next(error));
+        res.status(200).json(deletedCohort);
+    } catch (error) {
+        next(error);
+    }
 })
+
 module.exports = router;
--- server/routes/students.routes.js
+++ server/routes/students.routes.js
@@ -3,78 +3,85 @@ const router = require('express').Router();
 const Cohorts = require('../models/cohorts.model');
 const Students = require('../models/students.model');
 
-router.post("/students", (req, res, next) => {
-    Students.create({
-        ...req.body,
-    })
-        .then((student) => {
-            console.log("Created new student:", student);
-            res.status(201).json(student);
-        })
-        .catch((error) => next(error));
+router.post("/students", async (req, res, next) => {
+    try {
+        const student = await Students.create({
+            ...req.body,
+        });
+        console.log("Created new student:", student);
+        res.status(201).json(student);
+    }
+    catch (error) {
+        next(error);
+    }
+});
+
+router.get("/students", async (req, res, next) => {
+    try {
+        const student = await Students.find({}).populate("cohort");
+        res.status(200).json(student)
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get("/students/:studentId", async (req, res, next) => {
+    try {
+        const student = await Students.findById(req.params.studentId).populate("cohort");
+        if (!student) return res.status(404).json({ message: "Student not found" });
+        res.status(200).json(student);
+
+    } catch (error) {
+        next(error)
+    }
 })
 
 
 
-router.get("/students", (req, res, next) => {
-    Students.find({})
-        .populate("cohort")
-        .then((students) => {
-            console.log(`Found ${students.length}`);
-            res.json(students);
-        })
-        .catch((error) => next(error));
+router.get("/students/cohort/:cohortId", async (req, res, next) => {
+    try {
+        const students = await Students.find({ cohort: req.params.cohortId })
+            .populate("cohort");
+        console.log(`Found "${students.length}" in cohort ${req.params.cohortId}`);
+        res.status(200).json(students);
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+
+router.put("/students/:studentsId", async (req, res, next) => {
+    try {
+        const updatedStudent = await Students.findByIdAndUpdate(req.params.studentsId, req.body, { new: true })
+            .populate("cohort");
+
+        if (!updatedStudent) {
+            return res.status(404).json({ message: "Student not found" });
+        }
+
+        console.log("Updated student:", updatedStudent);
+        res.status(200).json(updatedStudent);
+    } catch (error) {
+        next(error);
+    }
 })
 
 
 
-router.get("/students/:studentsId", (req, res, next) => {
-    Students.findById(req.params.studentsId)
-        .populate("cohort")
-        .then((student) => {
-            if (!student) return res.status(404).json({ message: "Student not found" });
-            res.json(student);
-        })
-        .catch((error) => next(error));
-})
+router.delete("/students/:studentsId", async (req, res, next) => {
+    try {
+        const deletedStudent = await Students.findByIdAndDelete(req.params.studentsId)
 
+        if (!deletedStudent) {
+            return res.status(404).json({ message: "Student not found" });
+        }
 
-
-router.get("/students/cohort/:cohortId", (req, res, next) => {
-    Students.find({ cohort: req.params.cohortId })
-        .populate("cohort")
-        .then((students) => {
-            console.log(`Found "${students.length}" in cohort ${req.params.cohortId}`);
-            res.json(students);
-        })
-        .catch((error) => next(error));
-})
-
-
-
-router.put("/students/:studentsId", (req, res, next) => {
-    Students.findByIdAndUpdate(req.params.studentsId, req.body, { new: true })
-        .populate("cohort")
-        .then((updatedStudent) => {
-            if (!updatedStudent) return res.status(404).json({ message: "Student not found" });
-            console.log("Updated student:", updatedStudent);
-
-            res.json(updatedStudent);
-        })
-        .catch((error) => next(error));
-})
-
-
-
-router.delete("/students/:studentsId", (req, res, next) => {
-    Students.findByIdAndDelete(req.params.studentsId)
-        .then((deletedStudent) => {
-            if (!deletedStudent) return res.status(404).json({ message: "Student not found" });
-            console.log("Deleted student:", deletedStudent);
-
-            res.json(deletedStudent);
-        })
-        .catch((error) => next(error));
+        console.log("Deleted student:", deletedStudent);
+        res.status(200).json(deletedStudent);
+    } catch (error) {
+        next(error);
+    }
 })
 
 module.exports = router;
